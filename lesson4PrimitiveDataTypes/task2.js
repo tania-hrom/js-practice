df145081a088d4abc3c1296948069280a5f30a58
tania-hrom <tania.gromovaa@gmail.com>
2025-12-14 23:11:26 +0200

    Lesson 4 commit

---
+++ lesson4PrimitiveDataTypes/task2.js
@@ -0,0 +1,8 @@
+let firstPerson = "Petro";
+let secondPerson = "Ivan";
+
+let welcomeMessage = "Hi, dear " + firstPerson + " and " + secondPerson + "! Welcome!";
+console.log(welcomeMessage);
+
+let welcomeMessageTemplate = `Hi, dear ${firstPerson} and ${secondPerson}! Welcome!`;
+console.log(welcomeMessageTemplate);
---
+++ lesson4PrimitiveDataTypes/task3.js
@@ -0,0 +1,4 @@
+let age = (Math.random() * 100).toFixed(0);
+let isAdult = (age >= 18);
+
+console.log(`My age is ${age}. Is it true that I'm an adult? Actually, it's ${isAdult}`);
